b542cc9ada9ede5c1637ac760a827901d050e2be
Kayn Helga <32682968+kaynhelga9@users.noreply.github.com>
2024-06-24 07:33:30 +0700

    backend auth

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
@@ -9,11 +11,19 @@ const port = process.env.port || 5000;
 connectDB();
 const app = express();
 
+// Body parsing
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
 	res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
